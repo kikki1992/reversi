dd3528435b5fae5d8eec254335395788534d5710
松山佑真 <67245503+kikki1992@users.noreply.github.com>
2022-09-22 13:46:14 +0900

    board
    ボードの実装、石を置けるかどうか確認

--- オセロ/main.py
+++ オセロ/main.py
@@ -1,28 +1,77 @@
-from pydoc import visiblename
+from ast import While
+from pickle import FALSE, TRUE
+from tabnanny import check
+import numpy as np
 import pyxel
 from pyxelunicode import PyxelUnicode
 
-init_stage = [0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,2,1,0,0,0,
-        0,0,0,1,2,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,]
+# 0:空き枠、1:黒いし、-1: 白石、2:壁
+
+EMPTY = 0
+WHITE = -1
+BLACK = 1
+WALL = 2
+BOARD_SIZE = 8
+
+font_path = "assets/x14y20pxScoreDozer.ttf"
+font_size = 40
+
+pyuni = PyxelUnicode(font_path, font_size)
+
+class Board():
+    def __init__(self):
+        self.RawBoard = np.zeros((BOARD_SIZE+2, BOARD_SIZE+2),dtype=int)
+        #上の壁
+        self.RawBoard[0,:] = WALL
+        #左の壁
+        self.RawBoard[:,0] = WALL
+        #下の壁
+        self.RawBoard[BOARD_SIZE+1,:] = WALL
+        #右の壁
+        self.RawBoard[:,BOARD_SIZE+1] = WALL
+        
+        #初期位置
+        self.RawBoard[4,4] = WHITE
+        self.RawBoard[5,5] = WHITE
+        self.RawBoard[5,4] = BLACK
+        self.RawBoard[4,5] = BLACK
 
 
 class App():
     def __init__(self):
         #Windowの作成(最初に設定)
         pyxel.init(800,600,title="Reversi")
-        self.stage = init_stage
+        self.board = Board()
+        self.stage = self.board.RawBoard
+        self.turn = 1
+        self.row = 0
+        self.col = 0
+        self.check = False
         pyxel.mouse(True)
         pyxel.run(self.update,self.draw)
 
-        
+                     
+    def check_put(self,row,col):
+        if self.stage[row-1,col] == self.turn*-1:
+            return True
+        else:
+            return False
+
+
     def update(self):
-        pass
+
+        if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
+            self.x = pyxel.mouse_x
+            self.y = pyxel.mouse_y
+        
+            #盤面内をクリックした場合のアクション
+            if 150 < self.x < 630 and 50 < self.y < 530 :
+                self.row = (self.x - 90)//60 
+                self.col = (self.y +10)//60
+                if self.stage[self.row,self.col] == 0:
+                    self.stage[self.row,self.col] = self.turn
+                    self.check = self.check_put(self.row,self.col)
+                    self.turn *= -1
 
     def draw(self):
         pyxel.cls(1)
@@ -32,18 +81,17 @@ class App():
         for i in range(50,535,60):
             pyxel.line(150,i,630,i,0)
         
-        num = 0
-        for i in self.stage:
-            if i == 1:
-                row = num % 8
-                col = num // 8
-                pyxel.circ(180+row*60,80+col*60,20,0)
-            elif i == 2:
-                row = num % 8
-                col = num // 8
-                pyxel.circ(180+row*60,80+col*60,20,7)
-            num += 1
-            
+        for j in range(1,BOARD_SIZE+1):
+            for i in range(1,BOARD_SIZE+1):
+                if self.stage[i,j] == BLACK:
+                    pyxel.circ(120+i*60,20+j*60,20,0)
+                elif self.stage[i,j] == WHITE:
+                    pyxel.circ(120+i*60,20+j*60,20,7)
 
+        pyuni.text(0,1,"row{},col{}".format(self.row,self.col),0)
+        pyuni.text(0,40,"{}".format(self.stage[self.row,self.col]),0)
+        pyuni.text(0,80,"{}".format(self.check),0)
+        
 App()
+
 
