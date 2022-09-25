d274dc76f45479caaab085145bef7453cdeb2232
松山佑真 <67245503+kikki1992@users.noreply.github.com>
2022-09-25 10:32:34 +0900

    provisional finish
    オセロゲーム仮の完成
    ゲーム自体は実行可能
    コードを整理する前

--- オセロ/main.py
+++ オセロ/main.py
@@ -1,5 +1,6 @@
 from ast import While
 from email.quoprimime import body_encode
+from mimetypes import init
 from pickle import FALSE, TRUE
 from queue import Empty
 from tabnanny import check
@@ -31,7 +32,6 @@ class Board():
         self.RawBoard[BOARD_SIZE+1,:] = WALL
         #右の壁
         self.RawBoard[:,BOARD_SIZE+1] = WALL
-        self.CheckBoard = self.RawBoard
         #初期位置
         self.RawBoard[4,4] = WHITE
         self.RawBoard[5,5] = WHITE
@@ -45,12 +45,14 @@ class App():
         pyxel.init(800,600,title="Reversi")
         self.board = Board()
         self.stage = self.board.RawBoard
-        self.check_board = self.board.CheckBoard
         self.turn = 1
         self.row = 0
         self.col = 0
+        self.black_count = 2
+        self.white_count = 2
         self.check = False
         self.check_pos = self.available_pos()
+        self.flag = 0
         pyxel.mouse(True)
         pyxel.run(self.update,self.draw)
 
@@ -257,43 +259,107 @@ class App():
             self.y = pyxel.mouse_y
         
             #盤面内をクリックした場合のアクション
-            if 150 < self.x < 630 and 50 < self.y < 530 :
-                self.row = (self.x - 90)//60 
+            if 50 < self.x < 530 and 50 < self.y < 530 :
+                self.row = (self.x + 10)//60 
                 self.col = (self.y +10)//60
                 if self.stage[self.row,self.col] == 0:
                     self.check = self.put_stone(self.row,self.col)
                     if self.check == True:
                         self.turn *= -1
                         self.check_pos = self.available_pos()
+                        
+                        #黒のカウント
+                        self.black_count = 0
+                        self.white_count = 0
+                        for row in range(1,BOARD_SIZE+1):
+                            for col in range(1,BOARD_SIZE+1):
+                                if self.stage[row,col] == 1:
+                                    self.black_count += 1
+                        #白のカウント
+                                if self.stage[row,col] == -1:
+                                    self.white_count += 1
+        
+            #PATHをクリックした場合のアクション
+            if 550 < self.x < 690 and 400 < self.y < 460 and len(self.check_pos) == 0:
+                self.turn *= -1
+                self.check_pos = self.available_pos()
+                if len(self.check_pos) == 0:
+                    self.flag =1
+
+            if self.black_count + self.white_count == 64 or self.black_count == 0 or self.white_count == 0:
+                self.flag = 1
+            
+            if self.flag == 1 and 170 < self.x < 410 and 310 < self.y < 390:
+                self.flag = 0
+                self.board = Board()
+                self.stage = self.board.RawBoard
+                self.turn = 1
+                self.row = 0
+                self.col = 0
+                self.black_count = 2
+                self.white_count = 2
+                self.check = False
+                self.check_pos = self.available_pos()
+                
+
 
     def draw(self):
         pyxel.cls(1)
-        pyxel.rect(150,50,480,480,11)
-        for i in range(150,635,60):
+        pyxel.rect(50,50,480,480,11)
+        for i in range(50,535,60):
             pyxel.line(i,50,i,530,0)
         for i in range(50,535,60):
-            pyxel.line(150,i,630,i,0)
+            pyxel.line(50,i,530,i,0)
         
         for j in range(1,BOARD_SIZE+1):
             for i in range(1,BOARD_SIZE+1):
                 if self.stage[i,j] == BLACK:
-                    pyxel.circ(120+i*60,20+j*60,20,0)
+                    pyxel.circ(20+i*60,20+j*60,20,0)
                 elif self.stage[i,j] == WHITE:
-                    pyxel.circ(120+i*60,20+j*60,20,7)
+                    pyxel.circ(20+i*60,20+j*60,20,7)
 
         for i in self.check_pos:
-            pyxel.circ(120+i[0]*60,20+i[1]*60,5,2)
-
-        pyuni.text(0,1,"row{},col{}".format(self.row,self.col),0)
-        pyuni.text(0,40,"{}".format(self.stage[self.row,self.col]),0)
-        pyuni.text(0,80,"{}".format(self.check),0)
+            pyxel.circ(20+i[0]*60,20+i[1]*60,5,2)
 
         if self.turn == 1:
             s = "BLACK"
+            color = 0
+            B_color = 7
         else:
             s = "WHITE"
-
+            color = 7
+            B_color = 0
         
+        pyxel.rect(545,45,170,100,9)
+        pyxel.rect(550,50,160,90,B_color)
+    
+        pyuni.text(560,60,"TURN",color)
+        pyuni.text(560,100,"{}".format(s),color)
+
+        pyuni.text(560,180,"BLACK",7)
+        pyuni.text(560,220,"{}".format(self.black_count),7)
+        pyuni.text(560,300,"WHITE",7)
+        pyuni.text(560,340,"{}".format(self.white_count),7)
+
+        pyxel.rect(545,395,150,70,14)
+        pyxel.rect(550,400,140,60,13)
+        pyuni.text(560,410,"PATH",0)
+
+        if self.flag == 1 :
+            pyxel.rect(80,80,420,420,9)
+            pyxel.rect(90,90,400,400,7)
+
+            pyxel.rect(160,300,260,100,14)
+            pyxel.rect(170,310,240,80,13)
+            pyuni.text(200,330,"Restart",0)
+
+            if self.white_count>self.black_count:
+                pyuni.text(100,160,"WHITE Win!!",1)
+            elif self.white_count<self.black_count:
+                pyuni.text(100,160,"BLACK Win!!",1)
+            else:
+                pyuni.text(100,160,"EQUALL",1)
+
 App()
 
 
